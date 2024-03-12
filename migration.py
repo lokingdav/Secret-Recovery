85c6e02f95ecfc743b1409265c837ebe8683f962
David L. Adei <kofidahmed@gmail.com>
2024-03-12 15:26:48 +0000

    added table to hold posted transactions

--- migration.py
+++ migration.py
@@ -2,7 +2,7 @@ from skrecovery import database
     
 def migrate():
     print('Migrating database...')
-    database.drop_table(['pending_txs' 'ledgers'])
+    database.drop_table(['posted_txs', 'pending_txs', 'ledgers'])
     
     database.create_table('ledgers', [
         '`id` INT AUTO_INCREMENT PRIMARY KEY',
@@ -16,6 +16,14 @@ def migrate():
         'id INT AUTO_INCREMENT PRIMARY KEY', 
         'payload LONGTEXT NOT NULL', 
         'created_at TIMESTAMP'
+    ])
+    
+    database.create_table('posted_txs', [
+        'id INT AUTO_INCREMENT PRIMARY KEY', 
+        'txid VARCHAR(255) NOT NULL',
+        'block_id INT NOT NULL', 
+        'created_at TIMESTAMP',
+        'INDEX `txid` (`txid`)',
     ])
 
 if __name__ == "__main__":
