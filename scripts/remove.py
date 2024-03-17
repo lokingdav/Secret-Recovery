835c9db37253b0f08549db51a6ea480287f40dce
David L. Adei <kofidahmed@gmail.com>
2024-03-17 21:29:04 +0000

    temp: adding benchmarks to remove protocol

--- scripts/remove.py
+++ scripts/remove.py
@@ -1,23 +1,27 @@
 from skrecovery.client import Client
 from skrecovery.server import Server
 from enclave.response import EnclaveRes
+from skrecovery.helpers import Benchmark
 from scripts.misc import get_client, get_cloud
-from skrecovery.helpers import print_human_readable_json
 
-def main():
+def main(num_runs, test_name):
     client: Client = get_client()
     cloud: Server = get_cloud()
     
-    # Client part 1: Initiate remove request
-    remove_req: dict = client.init_remove()
+    client_bm: Benchmark = Benchmark('client', test_name)
+    cloud_bm: Benchmark = Benchmark('cloud', test_name)
+    enclave_bm: Benchmark = Benchmark('enclave', test_name)
     
-    # Cloud part 1: Process remove request
-    res: EnclaveRes = cloud.process_remove(remove_req)
-    
-    # Client part 2: Verify response
-    client.complete_remove(res)
-    
-    print_human_readable_json(res.serialize())
+    for i in range(num_runs):
+        # Client part 1: Initiate remove request
+        remove_req: dict = client.init_remove()
+        
+        # Cloud part 1: Process remove request
+        res: EnclaveRes = cloud.process_remove(remove_req)
+        
+        # Client part 2: Verify response
+        client.complete_remove(res)
+        
 
 if __name__ == "__main__":
-    main()
+    main(num_runs=100, test_name="remove")
