ab11664f45a6200fd0554d2e75e97ad4d3349993
David L. Adei <kofidahmed@gmail.com>
2024-03-23 20:47:52 +0000

    Added try catch for enclave starter

--- enclave-vsock.py
+++ enclave-vsock.py
@@ -2,16 +2,20 @@ import argparse
 from skrecovery.vsock import VsockServer
 
 def start_enclave(port: int):
-    server: VsockServer = VsockServer()
-    server.bind(port)
-    server.listen()
+    try:
+        server: VsockServer = VsockServer()
+        server.bind(port)
+        server.listen()
+    except Exception as e:
+        print(f"Failed to start enclave: {e}")
+        exit(1)
 
 def main():
     parser = argparse.ArgumentParser(prog='vsock-sample')
     parser.add_argument('-p', '--port', type=int, help='The local port to listen on.')
     args = parser.parse_args()
     port = args.port if args.port else 5005
-    start_enclave(args.port)
+    start_enclave(port)
 
 if __name__ == "__main__":
     main()
