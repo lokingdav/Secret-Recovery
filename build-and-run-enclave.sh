1194326d2999b1047f157e0472db6cb06aa69282
David L. Adei <dahmed@ncsu.edu>
2024-04-24 23:38:29 +0000

    remove enclave image

--- build-and-run-enclave.sh
+++ build-and-run-enclave.sh
@@ -6,3 +6,6 @@ nitro-cli build-enclave --docker-uri sk-enclave --output-file sk_enclave.eif
 
 # Assign 2 vCPUs and 4096 MiB memory to the enclave
 nitro-cli run-enclave --config enclave-config.json
+
+# Remove image to save space
+docker image rm sk-enclave
